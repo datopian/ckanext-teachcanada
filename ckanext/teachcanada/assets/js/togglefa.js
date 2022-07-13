30a0989b2198cc2fe47f7a72b212d0d862c85ae7
steveoni <steohenoni2@gmail.com>
2022-07-13 11:58:32 +0100

    fix hambuger menu

--- ckanext/teachcanada/assets/js/togglefa.js
+++ ckanext/teachcanada/assets/js/togglefa.js
@@ -3,4 +3,8 @@ $(document).ready(()=>{
   $(".ttoggle").click(()=>{
     $('#tgl').toggleClass('fa-bars fa-times');
   })
+
+  $(".ttoggle2").click(()=>{
+    $('#tgl2').toggleClass('fa-bars fa-times');
+  })
 })
