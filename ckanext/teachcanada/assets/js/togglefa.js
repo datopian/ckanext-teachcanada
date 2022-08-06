4f68426cf0b5b9ba8679dee1cfb94201a31893c2
steveoni <steohenoni2@gmail.com>
2022-08-06 16:46:08 +0100

    fix background picture stretch and hambuger

--- ckanext/teachcanada/assets/js/togglefa.js
+++ ckanext/teachcanada/assets/js/togglefa.js
@@ -18,4 +18,10 @@ $(document).ready(()=>{
     $("#searchicon").css("display", "none")
     $("#search").css("display", "block")
   })
+
+  $('.dropdown.keep-open').on({
+    "shown.bs.dropdown": function() { this.closable = false; },
+    "click":             function() { this.closable = true; },
+    "hide.bs.dropdown":  function() { return this.closable; }
+  });
 })
